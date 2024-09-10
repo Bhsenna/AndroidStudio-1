830f12fece146008e7ac89f118491fbef05bdd6f
Aluno <bernardo.h24@aluno.ifsc.edu.br>
2024-09-09 22:00:10 -0300

    Adicionado randomizer com valor minimo e maximo inputado pelo usuario

--- app/src/main/java/br/ifsc/edu/br/contador/MainActivity.java
+++ app/src/main/java/br/ifsc/edu/br/contador/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -18,14 +19,28 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button mybutton = findViewById(R.id.button);
-        mybutton.setText("0");
+        TextView textView = findViewById(R.id.textView);
+        EditText editTextInicial = findViewById(R.id.editTextNumInicial);
+        EditText editTextFinal = findViewById(R.id.editTextNumFinal);
+        mybutton.setText("Numero");
         mybutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Button b = (Button) view;
-                TextView t = findViewById(R.id.textView);
                 Random random = new Random();
-                b.setText(Integer.toString(random.nextInt(100)));
+                int num_inicial;
+                try {
+                    num_inicial = Integer.parseInt(editTextInicial.getText().toString().trim());
+                } catch (Exception e) {
+                    num_inicial = 0;
+                }
+                int num_final;
+                try {
+                    num_final = Integer.parseInt(editTextFinal.getText().toString().trim());
+                } catch (Exception e) {
+                    num_final = 1;
+                }
+                textView.setText(Integer.toString(random.nextInt(num_final+1 - num_inicial) + num_inicial));
             }
         });
     }
