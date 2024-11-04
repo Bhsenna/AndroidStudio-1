fbb3144eb34b2ac77001e70c0dc36beb05c86d17
hazime <bernardo.h24@aluno.ifsc.edu.br>
2024-11-04 20:23:33 -0300

    Planetas innitial

---
+++ listagem/src/main/java/com/ifsc/listagem/MainActivity.java
@@ -0,0 +1,40 @@
+package com.ifsc.listagem;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    String[] nomes = new String[] {"Picareta", "Jhones Dark", "Camili","Daniela","Thoninho"};
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView=findViewById(R.id.listView);
+
+        ArrayAdapter<String> adapter =new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                nomes
+        );
+        PlanetaDao planetaDao = new PlanetaDao();
+        PlanetaAdapter planetaAdapter = new PlanetaAdapter(this, R.layout.planeta_item, planetaDao);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,Integer.toString(position),Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
