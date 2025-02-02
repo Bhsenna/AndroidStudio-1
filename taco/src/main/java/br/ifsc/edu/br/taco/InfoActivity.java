f31eb2e0b37b66bb167e65442d93a1539b449140
bhsenna <bernardo.h24@aluno.ifsc.edu>
2025-02-02 20:24:17 -0300

    Taco - Display de Lipidios/Açucares

--- taco/src/main/java/br/ifsc/edu/br/taco/InfoActivity.java
+++ taco/src/main/java/br/ifsc/edu/br/taco/InfoActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,6 +19,7 @@ public class InfoActivity extends AppCompatActivity {
     int codigo;
     ImageButton btn_volta;
     ListView listview_preparos;
+    Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +52,7 @@ public class InfoActivity extends AppCompatActivity {
                                 "union " +
                             "SELECT codigo, cod_preparo, forma_preparo from vitaminas " +
                         ") where codigo = " + codigo;
-        Cursor cursor = banco.rawQuery(query, null);
+        cursor = banco.rawQuery(query, null);
         if (cursor.moveToNext()) {
             int indexCod = cursor.getColumnIndexOrThrow("cod_preparo");
             int indexForma = cursor.getColumnIndexOrThrow("forma_preparo");
@@ -65,5 +67,22 @@ public class InfoActivity extends AppCompatActivity {
             PreparoAdapter appAdapter = new PreparoAdapter(this, R.layout.item_lista, preparos);
             listview_preparos.setAdapter(appAdapter);
         }
+        cursor.close();
+
+        query = "SELECT * from alimentos_lip_acucares_100g " +
+                "where codigo = " + codigo;
+        cursor = banco.rawQuery(query, null);
+        if (cursor.moveToNext()) {
+            ((TextView) findViewById(R.id.colesterol_mg)).setText(String.valueOf(cursor.getFloat(cursor.getColumnIndexOrThrow("colesterol_mg"))));
+            ((TextView) findViewById(R.id.axidosgraxos_saturados_g)).setText(String.valueOf(cursor.getFloat(cursor.getColumnIndexOrThrow("axidosgraxos_saturados_g"))));
+            ((TextView) findViewById(R.id.axidosgraxos_monosaturados_g)).setText(String.valueOf(cursor.getFloat(cursor.getColumnIndexOrThrow("axidosgraxos_monosaturados_g"))));
+            ((TextView) findViewById(R.id.axidosgraxos_polisaturados_g)).setText(String.valueOf(cursor.getFloat(cursor.getColumnIndexOrThrow("axidosgraxos_polisaturados_g"))));
+            ((TextView) findViewById(R.id.axidograxos_linoleicos_g)).setText(String.valueOf(cursor.getFloat(cursor.getColumnIndexOrThrow("axidograxos-linoleicos_g"))));
+            ((TextView) findViewById(R.id.Axidograxos_linoleinos_g)).setText(String.valueOf(cursor.getFloat(cursor.getColumnIndexOrThrow("Axidograxos-linoleinos_g"))));
+            ((TextView) findViewById(R.id.axidograxo_trans_totais_g)).setText(String.valueOf(cursor.getFloat(cursor.getColumnIndexOrThrow("axidograxo-trans_totais_g"))));
+            ((TextView) findViewById(R.id.acucares_totais_g)).setText(String.valueOf(cursor.getFloat(cursor.getColumnIndexOrThrow("acucares_totais_g"))));
+            ((TextView) findViewById(R.id.acucares_adicao_g)).setText(String.valueOf(cursor.getFloat(cursor.getColumnIndexOrThrow("acucares_adicao_g"))));
+        }
+        cursor.close();
     }
 }
